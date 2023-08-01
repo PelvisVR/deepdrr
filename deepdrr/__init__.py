ae68771c6aadb56527e922957725740a6f68459e
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2023-08-01 18:46:06 -0400

    Switch to poetry

--- deepdrr/__init__.py
+++ deepdrr/__init__.py
@@ -1,3 +1,12 @@
+try:
+    import cupy
+except ImportError:
+    raise ImportError("""CuPy must be installed to use DeepDRR.
+Please install the version of CuPy for your CUDA Toolkit version by following the instructions here: https://cupy.dev/
+Or by installing deepdrr with the optional CuPy extra for your CUDA Toolkit version:
+e.g. pip install deepdrr[cupy11x] for CUDA v11.2 ~ 11.8
+""")
+
 from . import vis, geo, projector, device, annotations, utils
 from .projector import Projector
 from .vol import Volume, Mesh
